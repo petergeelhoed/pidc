932751e4e60a354a104744508d5f9617e6292494
petergeelhoed <petergeelhoed@gmail.com>
2025-02-24 10:21:29 +0100

    overflowprotection

--- findratio.c
+++ findratio.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,101 +17,98 @@ int main(int argc, char** argv)
     clock_t time_req = clock();
 
     int v = 0;
-    long loop = 1;
+    long a = 1;
     if (argc > 1)
     {
-        loop = atoi(argv[1]);
+        a = atoi(argv[1]);
     }
     if (argc > 2)
         v = atoi(argv[2]);
-    for (long a = loop; a <= loop; a++)
+    unsigned long maxn = sqrt(ULONG_MAX / a);
+    unsigned long maxt = sqrt(ULONG_MAX);
+    long remainder = a;
+    // unsigned long n = 182475670;
+    unsigned long n = 1;
+    unsigned long t = 0;
+    unsigned long count = 0;
+    while (remainder != 1 && remainder != 0)
     {
-        long remainder = a;
-        unsigned long n = 258059572;
-        // unsigned long n = 1;
-        unsigned long t = 0;
-        unsigned long count = 0;
-        while (remainder != 1 && remainder != 0)
+        if (n > maxn || t > maxt)
+        {
+            printf("Overflow %lu %lu\n", n, t);
+
+            exit(-15);
+        }
+        count++;
+        long unsigned int nSquareA = n * n * a;
+        t = sqrtl(nSquareA) + 0.5;
+        long unsigned int tSquare = t * t;
+
+        remainder = tSquare - nSquareA;
+        if (remainder == 0)
+            break;
+        unsigned long newt = tSquare + nSquareA;
+        if (newt < tSquare)
+        {
+            printf("Overflow SQ %lu %lu %lu %lu \n", n, t, newt, tSquare);
+
+            exit(-15);
+        }
+        long unsigned newn = n * t * 2;
+        if (v > 2)
+            printf("TT %ld %lu %lu %ld %lu %ld %lu %lu %lu\n",
+                   a,
+                   t,
+                   n,
+                   remainder,
+                   labs(remainder),
+                   newn % labs(remainder),
+                   tSquare,
+                   nSquareA,
+                   newt);
+
+        if (newn % labs(remainder) == 0)
         {
             count++;
-            long unsigned int nSquareA = n * n * a;
-            t = sqrtl(nSquareA) + 0.5;
-            long unsigned int tSquare = t * t;
-            if (t < n)
+            long g = gcd(newt, newn);
+            newt /= g;
+            newn /= g;
+            remainder = newt * newt - newn * newn * a;
+
+            if (v > 1)
+                printf(
+                    "TEST %ld %ld %ld %ld %ld\n", a, newt, newn, remainder, g);
+            if (remainder == 1)
             {
-                printf("Overflow %lu %lu %lu %lu %lu %Lf\n",
-                       t,
-                       n,
-                       nSquareA,
-                       tSquare,
-                       nSquareA + tSquare,
-                       sqrtl(nSquareA) + 0.5);
-                exit(-15);
-            }
-            remainder = tSquare - nSquareA;
-            if (remainder == 0)
+                n = newn;
+                t = newt;
                 break;
-            unsigned long newt = tSquare + nSquareA;
-            long unsigned newn = n * t * 2;
-            if (v > 2)
-                printf("TT %ld %lu %lu %ld %lu %ld %lu %lu %ld\n",
-                       a,
-                       t,
-                       n,
-                       remainder,
-                       labs(remainder),
-                       newn % labs(remainder),
-                       tSquare,
-                       nSquareA,
-                       newt);
-
-            if (newn % labs(remainder) == 0)
-            {
-                count++;
-                long g = gcd(newt, newn);
-                newt /= g;
-                newn /= g;
-                remainder = newt * newt - newn * newn * a;
-
-                if (v > 1)
-                    printf("TEST %ld %ld %ld %ld %ld\n",
-                           a,
-                           newt,
-                           newn,
-                           remainder,
-                           g);
-                if (remainder == 1)
-                {
-                    n = newn;
-                    t = newt;
-                    break;
-                }
-                else
-                {
-                    // this should never happen.
-                    printf("NO GO: %ld %ld %ld %ld %lu\n",
-                           a,
-                           newt,
-                           newn,
-                           remainder,
-                           n);
-                    exit(1);
-                }
             }
-            n++;
-            if (t == 1)
+            else
             {
-                printf("%ld %ld %ld %ld %ld\n", a, t, n, remainder, count);
+                // this should never happen.
+                printf("NO GO: %ld %ld %ld %ld %lu\n",
+                       a,
+                       newt,
+                       newn,
+                       remainder,
+                       n);
+                exit(1);
             }
         }
-        if (t > 0 || a == 0)
+        n++;
+        if (t == 1)
         {
             printf("%ld %ld %ld %ld %ld\n", a, t, n, remainder, count);
         }
-        else
-        {
-            printf("Error overflow!!!\n");
-        }
+    }
+    if (t > 0 || a == 0)
+    {
+        printf("%ld %ld %ld %ld %ld\n", a, t, n, remainder, count);
+    }
+    else
+    {
+        printf("Error overflow!!!\n");
     }
     if (v > 3)
         printf("time= %lu/%lu\n", -time_req + clock(), CLOCKS_PER_SEC);
