fa9b4fed5ece57c84f71d6549797e17a2959c3ee
petergeelhoed <petergeelhoed@gmail.com>
2025-03-18 14:29:20 +0100

    asci to binary

---
+++ a2b.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    int c = 29;
+    int shifted;
+    int one = 1;
+    int result;
+    while (c = getchar(), c != EOF) // && c != 10)
+    {
+        char out[9] = "00000000";
+        for (int pos = 0; pos < 8; pos++)
+        {
+            shifted = one << (pos);
+            result = c & shifted;
+            if (result == shifted)
+            {
+                out[7 - pos] = '1';
+            }
+        }
+        //        printf("%d %s\n", c, out);
+        printf("%s ", out);
+    }
+    puts("");
+}
